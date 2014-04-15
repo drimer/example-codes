de099359f315f3ba3ccfb4885f58ac2faa02decc
Alberto Aguilera <drimer.aaj@gmail.com>
2014-04-15 17:50:54 +0100

    django: Give to the template all objects from database

--- python/django/mysite/blog/views.py
+++ python/django/mysite/blog/views.py
@@ -6,7 +6,6 @@ from blog.models import BlogPost
 
 
 def archive(request):
-    post = BlogPost(title='Mock title', body='Mock body',
-                    timestamp=datetime.now())
+    posts = BlogPost.objects.all()
     return render_to_response('archive.html',
-                              {'posts': [post]})
+                              {'posts': posts})
