1575323ba7cac8c6127c28583bac2767c994a9cb
Alberto Aguilera <drimer.aaj@gmail.com>
2014-04-16 11:48:47 +0100

    django: Add a simple form to add blog posts

--- python/django/mysite/blog/urls.py
+++ python/django/mysite/blog/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('blog.views',
     (r'^$', 'archive'),
+    (r'^create/', 'create_blogpost'),
 )
--- python/django/mysite/blog/views.py
+++ python/django/mysite/blog/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 from blog.models import BlogPost
@@ -9,3 +10,13 @@ def archive(request):
     posts = BlogPost.objects.all()
     return render_to_response('archive.html',
                               {'posts': posts})
+
+
+def create_blogpost(request):
+    if request.method == 'POST':
+        post = BlogPost(title=request.POST.get('title'),
+                        body=request.POST.get('body'),
+                        timestamp=datetime.now())
+        post.save()
+
+    return HttpResponseRedirect('/blog/')
