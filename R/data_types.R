2be124c20e3ead5e1ed4b8086bd66928bde387b1
Alberto Aguilera <drimer.aaj@gmail.com>
2014-12-14 13:37:51 +0000

    R: Add more examples for data types

--- R/data_types.R
+++ R/data_types.R
@@ -82,3 +82,121 @@ $dim
      [,1] [,2] [,3] [,4] [,5]
 [1,]    1    3    5    7    9
 [2,]    2    4    6    8   10
+
+# Binding columns or rows
+> x <- 4:6
+> y <- 7:9
+> cbind(x, y)
+     x y
+[1,] 4 7
+[2,] 5 8
+[3,] 6 9
+> rbind(x, y)
+  [,1] [,2] [,3]
+x    4    5    6
+y    7    8    9
+
+
+
+################################## LISTS #######################################
+
+> list(5, FALSE, "c", 3+1i)
+[[1]]
+[1] 5
+
+[[2]]
+[1] FALSE
+
+[[3]]
+[1] "c"
+
+[[4]]
+[1] 3+1i
+
+
+################################## FACTORS #####################################
+
+> x <- factor(c("blue", "red", "red", "blue", "red"))
+> x
+[1] blue red  red  blue red 
+Levels: blue red
+> table(x)
+x
+blue  red 
+   2    3 
+> unclass(x)
+[1] 1 2 2 1 2
+attr(,"levels")
+[1] "blue" "red" 
+
+# Specifying the order of the levels
+> x <- factor(c("blue", "red", "red", "blue", "red"), levels = c("red", "blue"))
+> x
+[1] blue red  red  blue red 
+Levels: red blue
+> table(x)
+x
+ red blue 
+   3    2 
+> unclass(x)
+[1] 2 1 1 2 1
+attr(,"levels")
+[1] "red"  "blue"
+
+
+################################ MISSING VALUES ###############################
+
+> x <- c(TRUE, NA, 5, FALSE, NaN)
+> is.nan(x)
+[1] FALSE FALSE FALSE FALSE  TRUE
+> is.na(x)
+[1] FALSE  TRUE FALSE FALSE  TRUE
+
+
+################################ DATA FRAMES ##################################
+
+> x <- data.frame(foo = 1:4, bar = c(T, T, F, F))
+> x
+  foo   bar
+1   1  TRUE
+2   2  TRUE
+3   3 FALSE
+4   4 FALSE
+> nrow(x)
+[1] 4
+> ncol(x)
+[1] 2
+
+
+################################## NAMES ######################################
+
+# In a vector
+> x <- 1:3
+> names(x)
+NULL
+> names(x) = c("foo", "bar", "blah")
+> x
+ foo  bar blah 
+   1    2    3 
+> names(x)
+[1] "foo"  "bar"  "blah"
+
+# In a list
+> x <- list(a = 1, b = 2, c = 3)
+> x
+$a
+[1] 1
+
+$b
+[1] 2
+
+$c
+[1] 3
+
+# In a matrix
+> m <- matrix(1:4, nrow = 2, ncol = 2)
+> dimnames(m) <- list(c("a", "b"), c("c", "d"))
+> m
+  c d
+a 1 3
+b 2 4
