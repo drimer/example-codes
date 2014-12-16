7a9f6bd5ecc0b6ebbe609513b0f6293ef60da774
Alberto Aguilera <drimer.aaj@gmail.com>
2014-12-16 21:14:30 +0000

    R: Add more of example for R language

--- R/data_types.R
+++ R/data_types.R
@@ -200,3 +200,112 @@ $c
   c d
 a 1 3
 b 2 4
+
+
+###################### ACCESSING TO ELEMENTS IN LISTS ########################
+
+# Subsetting a vector
+> x <- c("a", "b", "c", "c", "b", "a")
+> x[1]
+[1] "a"
+> x[1:3]
+[1] "a" "b" "c"
+> x[x > "a"]
+[1] "b" "c" "c" "b"
+> u <- x > "a"
+> u
+[1] FALSE  TRUE  TRUE  TRUE  TRUE FALSE
+> x[u]
+[1] "b" "c" "c" "b"
+
+# Subsetting a matrix
+> x <- matrix(1:6, 2, 3)
+> x
+     [,1] [,2] [,3]
+[1,]    1    3    5
+[2,]    2    4    6
+> x[1, 2]
+[1] 3
+> x[2, 1]
+[1] 2
+> x[1, ]
+[1] 1 3 5
+> x[, 2]
+[1] 3 4
+
+# Subsetting a matrix, and getting a 1x1 matrix instead of the value
+> x <- matrix(1:6, 2, 3)
+> x[1, 2]
+[1] 3
+> x[1, 2, drop = FALSE]
+     [,1]
+[1,]    3
+
+# Same thing with a row
+> x <- matrix(1:6, 2, 3)
+> x[1, ]
+[1] 1 3 5
+> x[1, , drop = FALSE]
+     [,1] [,2] [,3]
+[1,]    1    3    5
+
+# Subsetting a list
+> x <- list(foo = 1:4, bar = 0.6)
+> x[1]  # Returns a list (that contains the sequence foo) because the original
+        # type of x is a list.
+$foo
+[1] 1 2 3 4
+
+> x[[1]]  # Returns just the element foo. It is not contained in a list, as
+          # before.
+[1] 1 2 3 4
+> x$bar  # Equivalent to doing x[["bar"]]
+[1] 0.6
+> x[["bar"]]
+[1] 0.6
+> x["bar"]
+$bar
+[1] 0.6
+
+# Subsetting nested elements of a list
+> x <- list(a = list(10, 12, 14), b = c(3.14, 2.81))
+> x[[c(1, 3)]]  # 3rd element of the 1st element in the list
+[1] 14
+> x[[1]][[3]]  # 3rd element of the 1st element in the list
+[1] 14
+
+# Removing NA values from a single vector
+> x <- c(1, 2, NA, 4, NA, 6)
+> bad <- is.na(x)
+> bad
+[1] FALSE FALSE  TRUE FALSE  TRUE FALSE
+> x[!bad]
+[1] 1 2 4 6
+
+# Removing NA values from multiple vectors
+> x <- c(1, 2, NA, 4, NA, 5)
+> y <- c("a", NA, "c", "d", "e", "f")
+> good <- complete.cases(x, y)
+> good
+[1]  TRUE FALSE FALSE  TRUE FALSE  TRUE
+> x[good]
+[1] 1 4 5
+> y[good]
+[1] "a" "d" "f"
+
+# Removing NA values from matrices
+> airquality
+     Ozone Solar.R Wind Temp Month Day
+[1,]    41     190  7.3   60     5   1
+[2,]    36     118  4.9   58     5   2
+[3,]    28     151  5.7   63     5   3
+[4,]    29     290  2.5   64     5   4
+[5,]    NA      NA 10.2   57     5   5
+[6,]    29      NA 15.0   59     5   6
+> good <- complete.cases(airquality)
+> airquality[good, ]
+     Ozone Solar.R Wind Temp Month Day
+[1,]    41     190  7.3   60     5   1
+[2,]    36     118  4.9   58     5   2
+[3,]    28     151  5.7   63     5   3
+[4,]    29     290  2.5   64     5   4
