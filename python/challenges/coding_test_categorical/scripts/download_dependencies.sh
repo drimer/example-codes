7b52aa1d07c4cb9fd1cecbec8eafb1e617f04eda
Alberto Aguilera <drimer.aaj@gmail.com>
2015-05-26 18:36:49 +0100

    Add setup for development with no internet connection

---
+++ python/challenges/coding_test_categorical/scripts/download_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+wget http://code.jquery.com/jquery-2.1.3.min.js jquery.js
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js client/bootstrap.js
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.cs client/bootstrap.css
+wget http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.js client/angular.js
