dde4bfb18d2b2a6a8205e26b052ecfe997b28f98
Alberto Aguilera <drimer.aaj@gmail.com>
2016-11-26 07:29:12 +0000

    Angular2 Heroes: Move hero detail template into its own file

--- javascript/angular2/angular-tour-of-heroes/app/hero-detail.component.ts
+++ javascript/angular2/angular-tour-of-heroes/app/hero-detail.component.ts
@@ -8,18 +8,9 @@ import { HeroService } from './hero.service';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-      <button (click)="goBack()">Back</button>
-    </div>
-  `
+  templateUrl: 'hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
