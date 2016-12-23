f65111c3a91e6437965c60b5b4687042ba6397e0
Alberto Aguilera <drimer.aaj@gmail.com>
2016-12-23 08:32:06 +0000

    Angular2 Heroes: Use promises when retrieving data from the service

--- javascript/angular2/angular-tour-of-heroes/app/app.component.ts
+++ javascript/angular2/angular-tour-of-heroes/app/app.component.ts
@@ -88,6 +88,6 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 }
