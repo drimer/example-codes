08217b1a034ee368e9aba305026c3c1e83e27f25
Alberto Aguilera <drimer.aaj@gmail.com>
2016-12-23 08:32:50 +0000

    Angular2 Heroes: Add routing to the app along with an empty dashboard

--- javascript/angular2/angular-tour-of-heroes/app/app.component.ts
+++ javascript/angular2/angular-tour-of-heroes/app/app.component.ts
@@ -5,7 +5,11 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
--- javascript/angular2/angular-tour-of-heroes/app/app.module.ts
+++ javascript/angular2/angular-tour-of-heroes/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ],
 })
