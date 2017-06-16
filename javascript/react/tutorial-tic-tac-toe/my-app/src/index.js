c10317c3591b8ca75a199ab1bb0cde77844e6c54
Alberto Aguilera <drimer.aaj@gmail.com>
2017-06-16 15:47:38 +0100

    React Tutorial: Use a state on Square and toggle on click

--- javascript/react/tutorial-tic-tac-toe/my-app/src/index.js
+++ javascript/react/tutorial-tic-tac-toe/my-app/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
