e4e57bb52f6bc6f1cf09e646ccb8326d55464795
Alberto Aguilera <drimer.aaj@gmail.com>
2017-06-17 10:31:26 +0100

    React Tutorial: Remove useless function wrapper

--- javascript/react/tutorial-tic-tac-toe/my-app/src/index.js
+++ javascript/react/tutorial-tic-tac-toe/my-app/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={() => props.onClick()}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
