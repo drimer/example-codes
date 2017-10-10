dd07872b50d43db127a4c8ce592693f3219c0aa8
Alberto Aguilera <drimer.aaj@gmail.com>
2017-10-10 22:33:41 +0100

    Django-React: Make the web nicer to look at

--- miniprojects/django_react/web_frontend/src/reminders/pages/ReminderListPage.jsx
+++ miniprojects/django_react/web_frontend/src/reminders/pages/ReminderListPage.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 
 import Header from '../../common/components/header/Header.js';
-import Subheader from '../../common/components/subheader/Subheader.js';
 import ReminderSummaryBox from '../components/reminder_summary_box/ReminderSummaryBox.js';
+import SettingsMenu from "../../common/components/header/SettingsMenu.js";
 
 
 function ReminderListPageTemplate(props) {
     return (
-        <div id="main-container">
-            <Header/>
-            <Subheader isUserAuthenticated={props.isUserAuthenticated}/>
+        <div>
+            <div id="main-container">
+                <Header/>
 
-            <div id="page-body">
-                {props.error &&
-                    <div class="error">{props.error}</div>
-                }
+                <div id="page-body">
+                    {props.error &&
+                        <div className="error">{props.error}</div>
+                    }
 
-                {props.reminders.map((reminder) =>
-                    <ReminderSummaryBox key={reminder.id} reminder={reminder} />
-                )}
+                    {props.reminders.map((reminder) =>
+                        <ReminderSummaryBox key={reminder.id} reminder={reminder} />
+                    )}
+                </div>
             </div>
+
+            <SettingsMenu/>
         </div>
     );
 }
