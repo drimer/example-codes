07a3ba3a3d8f01cf9ad792593f43c6c84120c68b
Alberto Aguilera <drimer.aaj@gmail.com>
2017-10-11 08:52:44 +0100

    ReactJS-Django: Make the web nicer to look at

---
+++ miniprojects/django_react/web_frontend/src/common/components/header/SettingsMenu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './SettingsMenu.css';
+
+
+export default function SettingsMenuTemplate(props) {
+    return (
+        <div id="user-menu">
+            <ul>
+                <li className="user-menu-title">
+                    Settings
+                </li>
+                <hr/>
+
+                {props.showLogout &&
+                <li className="user-menu-item">
+                    <Link to="/logout">Log out</Link>
+                </li>
+                }
+
+                {props.showLogin &&
+                <li className="user-menu-item">
+                    <Link to="/login">Log in</Link>
+                </li>
+                }
+            </ul>
+        </div>
+    );
+}
