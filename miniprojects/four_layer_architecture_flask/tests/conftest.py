d3af753ffd67f97130c9334f9b5f7117cc146450
Alberto Aguilera <drimer.aaj@gmail.com>
2017-11-12 13:29:00 +0000

    Add first end-to-end test (/get with multiple results)

--- miniprojects/four_layer_architecture_flask/tests/conftest.py
+++ miniprojects/four_layer_architecture_flask/tests/conftest.py
@@ -1,20 +1,36 @@
 import pytest
+from flask_sqlalchemy import SQLAlchemy
 
 from dependency_injection.container import DIContainer
 
 
-@pytest.yield_fixture(autouse=True)
-def test_flask_app():
-    return DIContainer().app
+@pytest.fixture
+def test_db():
+    return SQLAlchemy()
 
 
 @pytest.yield_fixture(autouse=True)
-def test_app_context(test_flask_app):
-    with test_flask_app.app_context() as context:
+def flask_app(test_db):
+    app = DIContainer().app
+
+    app.config['DATABASE'] = test_db
+    app.testing = True
+
+    return app
+
+
+@pytest.yield_fixture(autouse=True)
+def app_context(flask_app):
+    with flask_app.app_context() as context:
         yield context
 
 
 @pytest.yield_fixture(autouse=True)
-def test_request_context(test_app_context, test_flask_app):
-    with test_flask_app.test_request_context():
+def request_context(app_context, flask_app):
+    with flask_app.test_request_context():
         yield
+
+
+@pytest.fixture
+def flask_client(flask_app):
+    return flask_app.test_client()
